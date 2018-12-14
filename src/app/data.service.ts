1758480c71fbf8a2715d4dd04d463c18306a9eff
PierreMr <mauerpierre@gmail.com>
2018-12-14 10:38:17 +0100

    Finally merged :rocket:

---
+++ src/app/data.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataService {
+	result:any;
+	money:string;
+	limit:number;
+
+  	constructor(private _http: HttpClient) { }
+
+	getPricesToday() {
+		 return new Promise((resolve, reject) => {
+	      this
+	        ._http
+	        .get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DASH,IOT&tsyms=EUR')
+		        .toPromise()
+		        .then(response => {
+		        
+		          resolve(response)
+		        })
+		        .catch(reject)
+			})
+	}
+
+	getDayChart(money, limit) {
+		 return new Promise((resolve, reject) => {
+	      this
+	        ._http
+	        .get(`https://min-api.cryptocompare.com/data/histoday?fsym=${money}&tsym=USD&limit=${limit}`)
+		        .toPromise()
+		        .then(response => {	
+		          resolve(response.Data)
+		        })
+		        .catch(reject)
+			})
+	}
+}
+
+
+/* map(result => this.result = result.json()) */
